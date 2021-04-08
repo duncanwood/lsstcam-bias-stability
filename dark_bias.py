27d900a7b822a50b5c483cccdd1a090351832efd
Duncan Wood <duncan@ucsc.edu>
2021-04-08 17:15:44 +0000

    cleaned up unused code

--- dark_bias.py
+++ dark_bias.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import numpy as np
-#import time
 import matplotlib.pyplot as plt
 import pandas as pd
 import astropy.time
@@ -9,16 +8,9 @@ from lsst.daf.persistence import Butler
 import lsst.afw
 import lsst.eotest.image_utils as imutils
 from pathlib import Path
-#from mpl_toolkits.axes_grid1 import make_axes_locatable
-
-#from lsst.eotest.sensor.AmplifierGeometry import makeAmplifierGeometry
-#import lsst.ip.isr as isr
 
 import sklearn.linear_model
 from sklearn.decomposition import PCA
-#import scipy.optimize
-#from scipy.signal import fftconvolve
-#from scipy.ndimage import gaussian_filter
 
 import random
 from astropy.visualization import (MinMaxInterval, PercentileInterval, SqrtStretch,
@@ -26,9 +18,6 @@ from astropy.visualization import (MinMaxInterval, PercentileInterval, SqrtStret
 
 import pickle
 from astropy.stats import sigma_clip
-#import re
-#import pathlib
-#import csv
 
 
 
@@ -47,8 +36,6 @@ def remove_pca_both(this_bias_data, serial_pca, parallel_pca):
 
 def get_pixel_data(exp, amp):
     
-    #det = exp.getDetector()
-    #amp = det[amp_num]
     im = exp.getMaskedImage()[amp.getRawBBox()]
     arr_imaging  = im[amp.getRawDataBBox()].getImage().getArray()
     arr_parallel = im[amp.getRawParallelOverscanBBox()].getImage().getArray()
@@ -82,8 +69,6 @@ class PCA_Corrector:
         for exp in biases:
             bias_data.append(get_pixel_data(exp, self.amp))
         
-        #datatypes = [('masked_im', object), ('im_arr', object),('parallel_arr', object),('serial_arr', object), ('metadata', object)]
-        #bias_data_arr = np.asarray(bias_data,dtype=datatypes)
         bias_data_arr = pd.DataFrame(bias_data)
         
         serial_means = [mean_serial(this_bias_data) for this_bias_data in bias_data_arr.to_dict('records')]
@@ -99,7 +84,6 @@ def plot_superbias(superbias, nbiases, dataname, title=None, outname=None, show_
     if title is None:
         title = f'Superbias: mean of {nbiases} exposures post-PCA oscan in {dataname}'
     fig = plt.figure(dpi=150, facecolor='white')
-    #fig.set_facecolor('white')
     plt.subplot(211)
     norm = ImageNormalize(superbias, interval=PercentileInterval(99))
     plt.imshow(np.rot90(superbias),norm=norm, cmap='gist_heat')
